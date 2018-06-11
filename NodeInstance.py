0dfae84be76314bffea9e298fe405874ebbeb14e
Joshua Boniface <joshua@boniface.me>
2018-06-11 02:01:26 -0400

    Improve output

--- NodeInstance.py
+++ NodeInstance.py
@@ -204,8 +204,7 @@ class NodeInstance():
 
         # Display node information to the terminal
         ansiiprint.echo('{}{} keepalive{}'.format(ansiiprint.purple(), self.name, ansiiprint.end()), '', 't')
-        ansiiprint.echo('{0}CPUs:{1} {2}  {0}Free memory [MiB]:{1} {3}  {0}Used memory [MiB]:{1} {4}  {0}Load:{1} {5}'.format(ansiiprint.bold(), ansiiprint.end(), self.cpucount, self.memfree, self.memused, self.cpuload), '', 'c')
-        ansiiprint.echo('{}Active domains:{} {} [{}]'.format(ansiiprint.bold(), ansiiprint.end(), self.domains_count, ','.join(self.domain_list)), '', 'c')
+        ansiiprint.echo('{0}Active Domains:{1} {2}  {0}CPUs:{1} {3}  {0}Free memory [MiB]:{1} {4}  {0}Used memory [MiB]:{1} {5}  {0}Load:{1} {6}'.format(ansiiprint.bold(), ansiiprint.end(), self.domains_count, self.cpucount, self.memfree, self.memused, self.cpuload), '', 'c')
 
         # Update our local node lists
         for node_name in self.t_node:
