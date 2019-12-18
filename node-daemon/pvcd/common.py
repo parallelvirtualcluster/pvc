2b1b78622ecaa6d85d6fe69cf460a8066b7b8503
Joshua M. Boniface <joshua@boniface.me>
2019-12-18 12:06:40 -0500

    Fix invalid arping option
    It made little difference and didn't error, but was incorrect.

--- node-daemon/pvcd/common.py
+++ node-daemon/pvcd/common.py
@@ -119,7 +119,7 @@ def createIPAddress(ipaddr, cidrnetmask, dev):
         )
     )
     run_os_command(
-        'arping -P -U -W 0.2 -c5 -I {dev} -S {ip} {ip}'.format(
+        'arping -P -U -W 0.2 -c5 -i {dev} -S {ip} {ip}'.format(
             dev=dev,
             ip=ipaddr
         ),
