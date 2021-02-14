b33c0ab0e2d7184a6ca7665293c4be79a014ef42
Joshua M. Boniface <joshua@boniface.me>
2021-02-14 16:48:42 -0500

    Final final fix to snapshot ordering

--- daemon-common/ceph.py
+++ daemon-common/ceph.py
@@ -783,4 +783,4 @@ def get_list_snapshot(zk_conn, pool, volume, limit, is_fuzzy=True):
         else:
             snapshot_list.append({'pool': pool_name, 'volume': volume_name, 'snapshot': snapshot_name})
 
-    return True, sorted(snapshot_list, key=lambda x: str(x['name']))
+    return True, sorted(snapshot_list, key=lambda x: str(x['snapshot']))
