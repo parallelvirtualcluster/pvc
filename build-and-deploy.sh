38eeb78423b0761f48c0dfdd64a42c57f90d17ec
Joshua M. Boniface <joshua@boniface.me>
2024-01-10 14:22:12 -0500

    Improve output formatting for build-and-deploy
    Also restore the pvcnoded restart that was erroneously removed.

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -25,28 +25,29 @@ if [[ -n ${1} ]]; then
 fi
 
 HOSTS=( ${@} )
-echo "> Deploying to host(s): ${HOSTS[@]}"
+echo "Deploying to host(s): ${HOSTS[@]}"
 
 # Move to repo root if we're not
 pushd $( git rev-parse --show-toplevel ) &>/dev/null
 
 # Prepare code
-echo "Preparing code (format and lint)..."
+echo "> Preparing code (format and lint)..."
 ./format || exit 1
 ./lint || exit 1
 
 # Build the packages
-echo -n "Building packages..."
+echo -n "> Building packages..."
 version="$( ./build-unstable-deb.sh 2>/dev/null )"
 echo " done. Package version ${version}."
 
 # Install the client(s) locally
-echo -n "Installing client packages locally..."
+echo -n "> Installing client packages locally..."
 $SUDO dpkg -i --force-all ../pvc-client*_${version}*.deb &>/dev/null
 echo " done".
 
+echo "> Copying packages..."
 for HOST in ${HOSTS[@]}; do
-    echo -n "Copying packages to host ${HOST}..."
+    echo -n ">>> Copying packages to host ${HOST}..."
     ssh $HOST $SUDO rm -rf /tmp/pvc &>/dev/null
     ssh $HOST mkdir /tmp/pvc &>/dev/null
     scp ../pvc-*_${version}*.deb $HOST:/tmp/pvc/ &>/dev/null
@@ -57,21 +58,21 @@ if [[ -z ${KEEP_ARTIFACTS} ]]; then
 fi
 
 for HOST in ${HOSTS[@]}; do
-    echo "> Deploying packages to host ${HOST}"
-    echo -n "Installing packages..."
+    echo "> Deploying packages on host ${HOST}"
+    echo -n ">>> Installing packages..."
     ssh $HOST $SUDO dpkg -i --force-all /tmp/pvc/*.deb &>/dev/null
     ssh $HOST rm -rf /tmp/pvc &>/dev/null
     echo " done."
-    echo -n "Restarting PVC daemons..."
+    echo -n ">>> Restarting PVC daemons..."
     ssh $HOST $SUDO systemctl restart pvcapid &>/dev/null
     sleep 2
     ssh $HOST $SUDO systemctl restart pvcworkerd &>/dev/null
     sleep 2
     ssh $HOST $SUDO systemctl restart pvchealthd &>/dev/null
-#    sleep 2
-#    ssh $HOST $SUDO systemctl restart pvcnoded &>/dev/null
+    sleep 2
+    ssh $HOST $SUDO systemctl restart pvcnoded &>/dev/null
     echo " done."
-    echo -n "Waiting for node daemon to be running..."
+    echo -n ">>> Waiting for node daemon to be running..."
     while [[ $( ssh $HOST "pvc -q node list -f json ${HOST%%.*} | jq -r '.[].daemon_state'" 2>/dev/null ) != "run" ]]; do
         sleep 5
         echo -n "."
